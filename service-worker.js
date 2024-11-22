3642ad868789dd7f2c3de3694a72efa907485a71
Ersin Çelik <clk.ersinnn@gmail.com>
2024-11-22 15:01:05 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("iku-hesap-cache").then((cache) => {
+      return cache.addAll(["/", "/index.html", "/style.css", "/script.js", "/logo-192x192.png", "/logo-512x512.png"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
